212b85a44369bcf20c2ced8a3a7b3bbfbb16c040
Makito Eyamba <72389518+DannyMakito@users.noreply.github.com>
2026-05-28 11:51:19 +0200

    refactor(home): remove LogoTicker component
    Deprecate and remove the LogoTicker section from the homepage to simplify the layout.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import LogoTicker from '../components/LogoTicker';
 import WhyWeb from '../components/WhyWeb';
 import Metrics from '../components/Metrics';
 import Solutions from '../components/Solutions';
@@ -14,7 +13,6 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <LogoTicker />
       <WhyWeb />
       <Metrics />
       <Solutions />
